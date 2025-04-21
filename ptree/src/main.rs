acd637e62796cdce52ba82c02a0649752f2f031d
Victor Hjelmberg Feddersen <69452489+victorhjelmberg@users.noreply.github.com>
2025-04-21 18:36:01 +0200

    Code cleanup and refactor

--- ptree/src/main.rs
+++ ptree/src/main.rs
@@ -1,12 +1,23 @@
-mod token;
-mod ast;
+mod frontend;
+mod backend;
 
-use token::Token;
+use frontend::token::Token;
 use lalrpop_util::lalrpop_mod;
 use logos::Logos;
-use crate::ast::{Expr, Opcode};
+
+//Custom imports
+use crate::backend::backend::compile;
 
 lalrpop_mod!(pub calculator4);
+
+//Print the tokens received after lexical analysis
+fn print_tokens(tokens: &[(usize, Token, usize)]) {
+    print!("Tokens: ");
+    for (_, token, _) in tokens {
+        print!("{:?} ", token);
+    }
+    println!();
+}
 
 #[cfg(not(test))]
 fn main() {
@@ -24,50 +35,18 @@ fn main() {
             },
         })
         .collect();
+    
+    //Print tokens
+    print_tokens(&tokens);
 
     let parser = calculator4::ExprParser::new();
     match parser.parse(tokens.into_iter()) {
         Ok(ast) => {
             println!("Parse Tree: {:#?}", ast.to_raw());
-
-            let mut counter = 0;
-            let mut buffer = Vec::new(); 
-            let result_register = generate_llvm_ir(&ast, &mut counter, &mut buffer);
-
-            for line in buffer {
-                println!("{}", line);
-            }
-            println!("Result is in register: {}", result_register);
+            compile(&ast);
         }
         Err(e) => {
             eprintln!("Parse error: {:?}", e);
         }
-    }
-}
-
-
-fn generate_llvm_ir(ast: &Expr, counter: &mut u32, buffer: &mut Vec<String>) -> String {
-    match *ast {
-        Expr::Number(n) => format!("{}", n),
-        Expr::Op(ref left, op, ref right) => {
-            let left_ir = generate_llvm_ir(left, counter, buffer);
-            let right_ir = generate_llvm_ir(right, counter, buffer);
-
-            let op_ir = match op {
-                Opcode::Add => "add i32",
-                Opcode::Sub => "sub i32",
-                Opcode::Mul => "mul i32",
-                Opcode::Div => "div i32",
-            };
-
-            let reg_name = format!("%{}", *counter);
-            *counter += 1;
-
-            let op_result = format!("{} = {} {}, {}", reg_name, op_ir, left_ir, right_ir);
-            buffer.push(op_result);
-
-            reg_name
-        },
-        _ => String::from("error"),
     }
 }
