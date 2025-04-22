b183954bc5d9dda1c79643d3f0cf0a11da79e387
DL <83397267+StephanieB-lgtm@users.noreply.github.com>
2025-04-22 15:09:54 +0200

    Update lexer.rs
    Added newversion of lexer!

--- src/frontend/lexer.rs
+++ src/frontend/lexer.rs
@@ -2,60 +2,157 @@ use logos::Logos;
 use std::fmt;
 
 #[derive(Logos, Debug, PartialEq, Clone)]
-pub enum Token {
-    // Keywords
+//Define enum
+
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
+
+enum Token {
+    //First things first
+    //Whitespace - Ignore whitespace. \t is tab, \n is newline \f is form feed
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    Whitespace,
+
+    #[error] //error handler
+    Error,
+
+    #[regex(r"//[^\n]*", logos::skip)] //ignore oneline comments like this one
+
+    // Identifiers variables, or function names
+    #[regex("[a-zA-Z_][a-zA-Z_]*")]
+    Identifier,
+
+    //Operators
+    #[token("**")]
+    DoubleStar,
+
+    #[token("*")]
+    Star,
+
+    #[token("==")]
+    EqualsOperator,
+
+    #[token("=")]
+    AssignmentOperator,
+
+    #[token("+")]
+    Plus,
+
+    #[token("-")]
+    Minus,
+
+    #[token("/")]
+    Slash,
+
+    //Constants
+    #[regex("[0-9]+")]
+    IntegerNumbers,
+
+    #[regex(r"[0-9]+\.[0-9]+")]
+    DoubleNumbers,
+
+    //Keywords
     #[token("bool")]
     Boolean,
+
     #[token("int")]
     IntegerKeyword,
+
     #[token("double")]
     Double,
+
     #[token("string")]
     String,
+
+    #[token("table")]
+    Table,
+
+    #[token("row")]
+    Row,
+
+    #[token("fn")]
+    Function,
+
+    #[token("var")]
+    Var,
+
+    #[token("const")]
+    Constant,
+
     #[token("null")]
     Null,
-    #[token("<x>")]
-    Generic,
+
+    #[token("true")]
+    True,
+
+    #[token("false")]
+    False,
+
     #[token("if")]
     If,
+
     #[token("else")]
     Else,
 
-    // Symbols
-    #[token("=")]
-    AssignmentOperator,
-    #[token("==")]
-    EqualsOperator,
-    #[token("+")]
-    Plus,
-    #[token("-")]
-    Minus,
-    #[token("*")]
-    Star,
-    #[token("/")]
-    Slash,
+    #[token("while")]
+    While,
+
+    #[token("skip")] //equals to "pass" in python
+    Skip,
+
+    //Literals
+    #[regex(r#""([^"\\]|\\.)*""#)]
+    Stringliteral,
+
+    //Punctuators
     #[token(";")]
     Semicolon,
+
+    #[token("(")]
+    Openparan,
+
+    #[token(")")]
+    Closeparan,
+
+    #[token("{")]
+    Opencurlybracket,
+
+    #[token("}")]
+    Closecurlybracket,
+
+    #[token("[")]
+    Opensquarebracket,
+
+    #[token("]")]
+    Closesquarebracket,
+
+    #[token("<")] //FOR GENERICS to be handled at parsing, not lexical analysis
+    LeftAngle,
+
+    #[token(">")] //FOR GENERICS to be handled at parsing, not lexical analysis
+    RightAngle,
+
+    //Special chars
     #[token("!")]
     ExclamationMark,
+
     #[token("?")]
     QuestionMark,
-    #[token("(")]
-    LeftParen,
-    #[token(")")]
-    RightParen,
 
-    // Identifiers and numbers
-    #[regex("[a-zA-Z_][a-zA-Z_]*")]
-    Identifier,
-    #[regex(r"-?([0-9]+)", priority = 2, callback = parse_integer)]
-    IntegerNumber(i32),
-
-
-    // Whitespace
-    #[regex(r"[ \t\n\f]+", logos::skip)] // Skip whitespace
-    Whitespace,
+    #[token("$")]
+    Dollarsign,
 }
+
+fn main() {
+    let mut lex = Token::lexer("My mom said that 24 = 6 * 4");
+
+    while let Some(token) = lex.next() {
+        //Going through all tokens
+        println!("{:?} : {:?}", token, lex.slice());
+    }
+}
+
 
 pub fn print_tokens(tokens: &[(usize, Token, usize)]) {
     print!("Tokens: ");
@@ -64,6 +161,8 @@ pub fn print_tokens(tokens: &[(usize, Token, usize)]) {
     }
     println!();
 }
+
+
 /*
 pub fn collect_tokens(lexer: Token::Lexer) -> Vec<(usize, Token, usize)> {
     lexer
