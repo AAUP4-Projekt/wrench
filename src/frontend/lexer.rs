6837fa9e3df0d1ea0af0ea45e69de46c13d7553a
Victor Hjelmberg Feddersen <69452489+victorhjelmberg@users.noreply.github.com>
2025-04-23 14:43:44 +0200

    Added expanded lexer

--- src/frontend/lexer.rs
+++ src/frontend/lexer.rs
@@ -1,60 +1,172 @@
+//Define enum
+
 use logos::Logos;
 use std::fmt;
 
 #[derive(Logos, Debug, PartialEq, Clone)]
 pub enum Token {
-    // Keywords
+
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    Whitespace,
+    
+    /*
+    //First things first
+    //Whitespace - Ignore whitespace. \t is tab, \n is newline \f is form feed
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    Whitespace,
+
+    #[regex(r"//[^\n]*", logos::skip)] //ignore oneline comments like this one
+    */
+
+    // Identifiers variables, or function names
+    #[regex("[a-zA-Z_][a-zA-Z_]*", |lex| lex.slice().to_string())]
+    Identifier(String),
+
+    //Operators
+    #[token("**")]
+    Expon,
+
+    #[token("*")]
+    Star,
+
+    #[token("==")]
+    EqualsOperator,
+
+    #[token("=")]
+    AssignmentOperator,
+
+    #[token("+")]
+    Plus,
+
+    #[token("%")]
+    Modulo,
+
+    #[token("-")]
+    Minus,
+
+    #[token("/")]
+    Slash,
+
+    #[token("or")]
+    LogicalOr,
+
+    #[token("and")]
+    LogicalAnd,
+
+    //Constants
+    #[regex("[0-9]+", priority = 2, callback = parse_integer)] //Priority above identifiers
+    Integer(i32),
+
+    #[regex(r"[0-9]+\.[0-9]+")]
+    Doubleliteral,
+
+    //Keywords
     #[token("bool")]
     Boolean,
+
     #[token("int")]
     IntegerKeyword,
+
     #[token("double")]
-    Double,
+    DoubleKeyword,
+
     #[token("string")]
     String,
+
+    #[token("table")]
+    Table,
+
+    #[token("row")]
+    Row,
+
+    #[token("pipe")]
+    Pipe,
+
+    #[token("rpipe")]
+    Rpipe,
+
+    #[token("fn")]
+    Function,
+
+    #[token("var")]
+    Var,
+
+    #[token("const")]
+    Constant,
+
     #[token("null")]
     Null,
-    #[token("<x>")]
-    Generic,
+
+    #[token("true")]
+    True,
+
+    #[token("false")]
+    False,
+
     #[token("if")]
     If,
+
     #[token("else")]
     Else,
 
-    // Symbols
-    #[token("=")]
-    AssignmentOperator,
-    #[token("==")]
-    EqualsOperator,
-    #[token("+")]
-    Plus,
-    #[token("-")]
-    Minus,
-    #[token("*")]
-    Star,
-    #[token("/")]
-    Slash,
+    #[token("while")]
+    While,
+
+    #[token("skip")] //equals to "pass" in python
+    Skip,
+
+    //Literals
+    #[regex(r#""([^"\\]|\\.)*""#)]
+    Stringliteral,
+
+    //Punctuators
     #[token(";")]
     Semicolon,
+
+    #[token(",")]
+    Comma,
+
+    #[token("(")]
+    Openparan,
+
+    #[token(")")]
+    Closeparan,
+
+    #[token("{")]
+    Opencurlybracket,
+
+    #[token("}")]
+    Closecurlybracket,
+
+    #[token("[")]
+    Opensquarebracket,
+
+    #[token("]")]
+    Closesquarebracket,
+
+    #[token("<")] //FOR GENERICS to be handled at parsing, not lexical analysis
+    LeftAngle,
+
+    #[token(">")] //FOR GENERICS to be handled at parsing, not lexical analysis
+    RightAngle,
+
+    //Special chars
     #[token("!")]
     ExclamationMark,
+
     #[token("?")]
     QuestionMark,
-    #[token("(")]
-    LeftParen,
-    #[token(")")]
-    RightParen,
 
+    #[token("$")]
+    Dollarsign,
+
+    /*
     // Identifiers and numbers
     #[regex("[a-zA-Z_][a-zA-Z_]*", |lex| lex.slice().to_string())]
     Identifier(String),
     #[regex(r"-?([0-9]+)", priority = 2, callback = parse_integer)]
     IntegerNumber(i32),
-
-
-    // Whitespace
-    #[regex(r"[ \t\n\f]+", logos::skip)] // Skip whitespace
-    Whitespace,
+    */
 }
 
 fn parse_integer(lex: &mut logos::Lexer<Token>) -> i32 {
