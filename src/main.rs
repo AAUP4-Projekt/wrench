c7f78404308b4278456681cbaa51973e01a63f95
Victor Hjelmberg Feddersen <69452489+victorhjelmberg@users.noreply.github.com>
2025-04-24 16:27:21 +0200

    Added var token in variable declarations

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod frontend;
 
 //#[cfg(not(test))]
 fn main() {
-    let input = "int x = 7; x = 5; //Hello\n int y = (3 ** 2 ** 1);";
+    let input = "var int x = 7; x = 5; //Hello\n var int y = (3 ** 2 ** 1);";
     println!("Program: {}\n\nParsing:", input);
     create_ast(input);
 }
